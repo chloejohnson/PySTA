84e2aa314f71b0dd3b9ebe29b0fe413f41883b58
marceitner <47329621+marceitner@users.noreply.github.com>
2019-05-01 10:21:13 -0500

    Add files via upload
    Added the full 'saveData' method to the class 'Measurement'

--- read_analog_multichannel.py
+++ read_analog_multichannel.py
@@ -2,62 +2,84 @@
 
 
 import time
-
+import numpy as np
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 
 
 class Measurement():
-	'''
-		This is an abstract class that contains all options for a specific measurement run. For each new run an object of this class is created 
-		and all data is saved as its attribute.
+    '''
+        This is an abstract class that contains all options for a specific measurement run. For each new run an object of this class is created 
+        and all data is saved as its attribute.
 	
 	'''
-	def __init__(self, fs = 100):
+    def __init__(self, fs = 100):
 		# The object is created with sampling frequency fs, and empty lists for time and data.
-		self.time = []
-		self.t_init = time.time()
-		self.data = []
-		self.fs = fs
-		self.active_channels = active_channels
+        self.time = []
+        self.t_init = time.time()
+        self.data = []
+        self.fs = fs
+        
 		
-	def setData(self,values):
+    def setData(self,values):
 	# The values for all 8 channels are added to the 'data' attribute of the measurement object
-		for i in values:
-			self.data.append(values)
-		return 	
+        for i in values:
+            self.data.append(values)
+        return 	
 		
-	def setTime(self,t0):
+    def setTime(self,t0):
 	# The values for the current time (relative to the start time 't_init' is added to the 'time' attribute of the measurement object
 		
-		self.time.append(t0-self.t_init)
-		return 	
+        self.time.append(t0-self.t_init)
+        return 	
 		
-	def getData(self):
-	# This method returns the data in a structured way, for use in an animation or plot.
-		ch0_data = self.data[0:-1:8]
-		ch1_data = self.data[1:-1:8]
-		# ch2_data = data[2:-1:len(self.active_channels)]
-		# ch3_data = data[3:-1:len(self.active_channels)]
-		# ch4_data = data[4:-1:len(self.active_channels)]
-		# ch5_data = data[5:-1:len(self.active_channels)]
-		# ch6_data = data[6:-1:len(self.active_channels)]
-		# ch7_data = data[7:-1:len(self.active_channels)]
-			
-		return ch0_data,ch1_data
+    		
+    
 		
-	def filterData(self,filter_type):
+    def filterData(self,filter_type):
 	# Future work, possible implementation of FIR or IIR filters	
-		raise NotImplementedError
+        raise NotImplementedError
     
     def saveData(self,type='time',name='testfile'):
-        file = open(name,'w+')
-
-        file.write('| time | Chann0 | Chann1 | Chann2 | Chann3 | Chann4 | Chann5 | Chann6 | Chann7 |\n')
-        for i in range(0,len(time)):
-            file.write('| {8:.2f} | {0:.4f} | {1:.4f} | {2:.4f} | {3:.4f} | {4:.4f} | {5:.4f} | {6:.4f} | {7:.4f} |\n'.format(*self.data[i],self.time[i]))
-        file.close()
+        file = open(name+'.txt','w+')
+        if type == 'time':
+            file.write('| time | Chann0 | Chann1 | Chann2 | Chann3 | Chann4 | Chann5 | Chann6 | Chann7 |\n')
+            for i in range(0,len(self.data)):
+                file.write('| {8:.2f} | {0:.4f} | {1:.4f} | {2:.4f} | {3:.4f} | {4:.4f} | {5:.4f} | {6:.4f} | {7:.4f} |\n'.format(*self.data[i],self.time[i]))
+            file.close()
+        else:
+            file.write('| freq | Chann0 | Chann1 | Chann2 | Chann3 | Chann4 | Chann5 | Chann6 | Chann7 |\n')
+            x0 = []
+            x1 = []
+            x2 = []
+            x3 = []
+            x4 = []
+            x5 = []
+            x6 = []
+            x7 = []
+            for i in self.data:
+                x0.append(i[0])
+                x1.append(i[1])
+                x2.append(i[2])
+                x3.append(i[3])
+                x4.append(i[4])
+                x5.append(i[5])
+                x6.append(i[6])
+                x7.append(i[7])
+            FFT = []
+            FFT[0] = list(np.fft.fft(x0))
+            FFT[1] = list(np.fft.fft(x1))
+            FFT[2] = list(np.fft.fft(x2))
+            FFT[3] = list(np.fft.fft(x3))
+            FFT[4] = list(np.fft.fft(x4))
+            FFT[5] = list(np.fft.fft(x5))
+            FFT[6] = list(np.fft.fft(x6))
+            FFT[7] = list(np.fft.fft(x7))
+            frequency = list(np.linspace(0,self.fs/2,len(self.data)))
+            for i in range(0,len(time)):
+                file.write('| {8:.2f} | {0:.4f} | {1:.4f} | {2:.4f} | {3:.4f} | {4:.4f} | {5:.4f} | {6:.4f} | {7:.4f} |\n'.format(*FFT[i],frequency[i]))
+            file.close()
         return
         
         
